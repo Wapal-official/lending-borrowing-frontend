fa8b42f2a4d4d2516d6c835a0b559c83be2d19f8
Meghant00 <dangolmeghant0@gmail.com>
2023-09-11 16:59:54 +0545

    chore: add background

--- components/Navbar/Navbar.vue
+++ components/Navbar/Navbar.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="tw-w-full tw-flex tw-flex-row tw-items-center tw-justify-between tw-px-8 tw-py-6 tw-border tw-border-b-black md:tw-px-[3.75rem]"
+    class="tw-w-full tw-flex tw-flex-row tw-items-center tw-justify-between tw-px-8 tw-py-6 tw-border tw-border-b-black tw-absolute tw-top-0 tw-bg-transparent md:tw-px-[3.75rem]"
   >
     <a href="/"><img :src="logo" alt="logo" width="120px" height="35px" /></a>
     <connect-wallet />
